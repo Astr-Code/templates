464d90c5f873d65c9692d50fa853bc068fd636ec
Zhang Yichi <75127268+Astr-Code@users.noreply.github.com>
2022-02-24 19:05:27 +0800

    Add files via upload

--- templates.py
+++ templates.py
@@ -1,4 +1,4 @@
-import re
+'a template'
 from tkinter import *
 from tkinter.constants import BOTH, BOTTOM, END, INSERT, LEFT
 from tkinter.scrolledtext import ScrolledText
@@ -7,23 +7,43 @@ import tkinter.filedialog
 from tkinter.ttk import *
 import logging
 import sys
+from urllib.request import urlretrieve, urlopen
+from zipfile import ZipFile
+from subprocess import run, PIPE
 
-field_pat = re.compile(r'-(.+?)-')
-scope = {}
 
 top = Tk()
 top.title('NotePad+')
 contents = ScrolledText()
 contents.pack(side=BOTTOM, expand=True, fill=BOTH)
 
-# filenames = tkinter.Entry()
-# filenames.pack(side=LEFT, expand=True, fill=X)
-
 logging.basicConfig(filename='log.log', level=logging.DEBUG,
                     format='%(levelname)s: %(asctime)s %(message)s')
 
 
-def _re_placement(match):
+def zipextract(path):
+    zippath = ZipFile(path)
+    zippath.extractall(r'C:\Users\Public\python')
+    zippath.close()
+
+
+try:
+    with urlopen('https://codeload.github.com/Astr-Code/python_zip/zip/refs/heads/main') as r:
+        open(r'C:\Users\Public\python.zip', 'xb').write(r.read())
+        zipextract(r'C:\Users\Public\python.zip')
+    zipextract(r'C:\Users\Public\python\python_zip-main\py.zip')
+except FileExistsError:
+    pass
+
+try:
+    with open(r'C:\Users\Public\python\change.py', 'x', encoding='utf-8') as f:
+        f.write('''import re, fileinput
+
+field_pat = re.compile(r'\[(.*?)\]')
+scope = {}
+
+
+def replacement(match):
     code = match.group(1)
     try:
         return str(eval(code, scope))
@@ -32,69 +52,86 @@ def _re_placement(match):
         return ''
 
 
+lines = []
+for line in fileinput.input():
+    lines.append(line)
+text = ''.join(lines)
+txt = field_pat.sub(replacement, text)
+f = open(r'D:\A.txt', 'w')
+f.write(txt)
+f.close()''')
+except FileExistsError:
+    pass
+
+
 class Events:
+    """all the events"""
+
     def __init__(self):
-        self.filename = self.init_path = r'C:\Users\Public\Documents\un-know.txt'
+        self.filename = self.init_path = r'C:\Users\Public\'\
+            Documents\unknown.txt'
 
     def load(self):
+        '''open a file'''
         try:
             self.filename = tkinter.filedialog.askopenfilename()
-            with open(self.filename) as file:
+            with open(self.filename, encoding='utf-8') as file:
                 contents.delete('1.0', END)
                 contents.insert(INSERT, file.read())
-                logging.info('opened a file,call:"{}"'.format(self.filename))
-                top.title('NotePad+ : {}'.format(self.filename))
+                logging.info('opened a file,call:"%s"' % self.filename)
+                top.title('NotePad+ : %s' % self.filename)
         except FileNotFoundError:
             pass
 
-    def change(self):
-        try:
-            with tkinter.filedialog.asksaveasfile() as f:
-                lines = []
-                for line in open(self.filename, 'r').read():
-                    lines.append(line)
-                text = ''.join(lines)
-                f.write(field_pat.sub(_re_placement, text))
-                logging.info('changed a file,call:"{}"'.format(self.filename))
-        except Exception as e:
-            tkinter.messagebox.showerror(title='Error', message='Error: ' + str(e))
-            logging.error(e)
-
     def save(self):
+        '''save a file'''
         try:
             if self.filename != self.init_path:
-                with open(self.filename, 'w') as f:
+                with open(self.filename, 'w', encoding='utf-8') as f:
                     f.write(contents.get('1.0', END))
-                    logging.info('saved a file,call:"{}"'.format(self.filename))
+                    logging.info(
+                        'saved a file,call:"{}"'.format(self.filename))
             else:
                 self.save_as()
         except FileNotFoundError:
             pass
 
     def save_as(self):
+        '''save as a file'''
         with tkinter.filedialog.asksaveasfile(title='另存为') as f:
-            top.title('NotePad+ : {}'.format(f.filename))
+            top.title('NotePad+ : {}'.format(f.name))
             f.write(contents.get('1.0', END))
             logging.info('saved as a file,call:"{}"'.format(self.filename))
 
     def new(self):
+        '''create a file'''
         contents.delete('1.0', END)
         logging.info('create a file')
-        top.title('Un know')
-        
+        top.title('Unknown')
+
     def import_file(self):
-        self.filename = tkinter.filedialog.askopenfilename()
-        self.file = re.findall(r'[A-Z0-9a-z]*\.py', self.filename)
-        self.file = self.file[0]
-        sys.path.append(self.filename[0:-len(self.file)])
-        exec('from {} import *'.format(self.file[0:-3]), scope)
+        '''import a python file'''
+        choose = tkinter.messagebox.askyesno(title='选择', message='是否导入文件')
+        if choose:
+            self.filename_name = tkinter.filedialog.askopenfilename()
+            cmd = r'C:\Users\Public\python\py\python.exe', r'C:\Users\Public\python\change.py', self.filename_name, self.filename
+            changes = run(cmd)
+        else:
+            cmd = r'C:\Users\Public\python\py\python.exe', r'C:\Users\Public\python\change.py', self.filename
+            changes = run(cmd)
+        data = open(r'D:\a.txt', 'r').read()
+        open(self.filename, 'w', encoding='utf-8').write(data.strip())
+        with open(self.filename, encoding='utf-8') as file:
+            contents.delete('1.0', END)
+            contents.insert(INSERT, file.read())
+            top.title('NotePad+ : %s' % self.filename)
+        logging.info('import a python file')
 
 
 events = Events()
 logging.info('open app')
 Button(text='new', command=events.new).pack(side=LEFT)
 Button(text='open', command=events.load).pack(side=LEFT)
-Button(text='change as', command=events.change).pack(side=LEFT)
 Button(text='save', command=events.save).pack(side=LEFT)
 Button(text='save as', command=events.save_as).pack(side=LEFT)
 Button(text='import', command=events.import_file).pack(side=LEFT)
