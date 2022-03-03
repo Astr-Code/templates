4b5742a67599a69a6a02568cd2d180fce13492e9
Zhang Yichi <75127268+Astr-Code@users.noreply.github.com>
2022-03-03 19:34:10 +0800

    Add files via upload

--- templates.py
+++ templates.py
@@ -8,16 +8,14 @@ from tkinter import *
 from tkinter.constants import BOTH, BOTTOM, END, INSERT, LEFT
 from tkinter.scrolledtext import ScrolledText
 from tkinter.ttk import *
-from urllib.request import urlopen, urlretrieve
+from urllib.request import urlopen
 from zipfile import ZipFile
-import threading
 from pyperclip import copy, paste
 
 top = Tk()
 top.title('NotePad+')
-contents = ScrolledText()
-contents.pack(side=BOTTOM, expand=True, fill=BOTH)
-
+notebook = Notebook(top)
+top.geometry('800x600')
 logging.basicConfig(filename='log.log', level=logging.DEBUG,
                     format='%(levelname)s: %(asctime)s %(message)s')
 
@@ -78,8 +76,12 @@ class Events:
         try:
             self.filename = tkinter.filedialog.askopenfilename()
             with open(self.filename, encoding='utf-8') as file:
-                contents.delete('1.0', END)
-                contents.insert(INSERT, file.read())
+                tab1 = Frame(top)
+                self.contents = ScrolledText(tab1)
+                self.contents.insert(INSERT, file.read())
+                self.contents.pack(side=BOTTOM, expand=True, fill=BOTH)
+                notebook.add(tab1, text=self.filename)
+                notebook.select(tab1)
                 logging.info('opened a file,call:"%s"' % self.filename)
                 top.title('NotePad+ : %s' % self.filename)
         except FileNotFoundError:
@@ -87,10 +89,11 @@ class Events:
 
     def save(self, events=None):
         '''save a file'''
+        file = notebook.tab(notebook.select())
         try:
-            if self.filename != self.init_path:
-                with open(self.filename, 'w', encoding='utf-8') as f:
-                    f.write(contents.get('1.0', END))
+            if file != self.init_path:
+                with open(file.get('text'), 'w', encoding='utf-8') as f:
+                    f.write(self.contents.get('1.0', END))
                     logging.info(
                         'saved a file,call:"{}"'.format(self.filename))
             else:
@@ -102,56 +105,70 @@ class Events:
         '''save as a file'''
         with tkinter.filedialog.asksaveasfile(title='另存为') as f:
             top.title('NotePad+ : {}'.format(f.name))
-            f.write(contents.get('1.0', END))
+            f.write(self.contents.get('1.0', END))
             logging.info('saved as a file,call:"{}"'.format(self.filename))
 
     def new(self, events=None):
         '''create a file'''
-        contents.delete('1.0', END)
+        tab1 = Frame(top)
+        self.contents = ScrolledText(tab1)
+        self.contents.pack(side=BOTTOM, expand=True, fill=BOTH)
+        notebook.add(tab1, text='New Tab')
+        notebook.select(tab1)
         logging.info('create a file')
         top.title('Unknown')
 
+    def quit_tab(self, events=None):
+        """quit a tab"""
+        notebook.forget(notebook.select())
+        logging.info('quit a tab')
+
     def import_file(self, events=None):
         '''import a python file'''
+        file = notebook.tab(notebook.select())
         self.filename_name = tkinter.filedialog.askopenfilename()
-        cmd = r'C:\Users\Public\python\py\python.exe', r'C:\Users\Public\python\change.py', self.filename_name, self.filename
+        cmd = r'C:\Users\Public\python\py\python.exe', r'C:\Users\Public\python\change.py', self.filename_name, file.get('text')
         changes = run(cmd)
         self._after_change()
 
     def change(self, events=None):
         """change a file"""
-        cmd = r'C:\Users\Public\python\py\python.exe', r'C:\Users\Public\python\change.py', self.filename
+        file = notebook.tab(notebook.select())
+        cmd = r'C:\Users\Public\python\py\python.exe', r'C:\Users\Public\python\change.py', file.get('text')
         changes = run(cmd)
         self._after_change()
 
     def _after_change(self, events=None):
         """after change a file"""
+        file = notebook.tab(notebook.select())
         data = open(r'D:\a.txt', 'r').read()
-        open(self.filename, 'w', encoding='utf-8').write(data.strip())
+        open(file.get('text'), 'w', encoding='utf-8').write(data.strip())
         with open(self.filename, encoding='utf-8') as file:
-            contents.delete('1.0', END)
-            contents.insert(INSERT, file.read())
+            self.contents.delete('1.0', END)
+            self.contents.insert(INSERT, file.read())
             top.title('NotePad+ : %s' % self.filename)
         logging.info('change a file')
 
     def copy_text(self, events=None):
         """copy text"""
-        text = contents.get('sel.first', 'sel.last')
+        text = self.contents.get('sel.first', 'sel.last')
         copy(text)
 
     def paste_text(self, events=None):
         """paste text"""
         text = paste()
-        contents.insert(INSERT, text)
+        self.contents.insert(INSERT, text)
 
     def cut_text(self, events=None):
         """cut text"""
         self.copy_text(events)
-        contents.delete('sel.first', 'sel.last')
+        self.contents.delete('sel.first', 'sel.last')
 
 
 events = Events()
 logging.info('open app')
+events.new()
+notebook.pack(side=LEFT, expand=True, fill=BOTH)
 menubar = Menu(top)
 filemenu = Menu(menubar, tearoff=0)
 filemenu.add_command(label='新建', command=events.new, accelerator='Ctrl+N')
@@ -160,12 +177,15 @@ filemenu.add_command(label='保存', command=events.save, accelerator='Ctrl+S')
 filemenu.add_command(label='另存为', command=events.save_as,
                      accelerator='Ctrl+Shift+S')
 filemenu.add_separator()
+filemenu.add_command(
+    label='退出tab', command=events.quit_tab, accelerator='Ctrl+Q')
 filemenu.add_command(label='退出', command=top.quit)
 menubar.add_cascade(label='文件', menu=filemenu)
 top.bind("<Control-n>", events.new)
 top.bind("<Control-o>", events.load)
 top.bind("<Control-s>", events.save)
 top.bind("<Control-Shift-s>", events.save_as)
+top.bind("<Control-q>", events.quit_tab)
 
 editmenu = Menu(menubar, tearoff=0)
 editmenu.add_command(label='复制', command=events.copy_text,
